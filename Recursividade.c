345c3b7147bb0f5e24dc4adea1b2efcde54d7221
atlnery <49240604+atlnery@users.noreply.github.com>
2020-03-12 13:07:40 -0300

    add conversão de base

--- Recursividade.c
+++ Recursividade.c
@@ -7,26 +7,61 @@ int rec_fat(int n);
 int rec_mult(int a, int b);
 int rec_divide(int a, int b);
 int rec_fibo(int p);
+int rec_convertebase(int dec, int base); //precisou do return. Pru quê?
 
 //funções iterativas
 int it_fat(int n);
 int it_mult(int a, int b);
 int it_divide(int a, int b);
 int it_fibo(int p);
+void it_convertebase(int dec, int base);
+
+
+// conversão apenas de decimal para outras bases. 
+int rec_convertebase(int dec, int base){
+    
+    if (dec < base){         
+        printf("%d", dec); 
+        return 0;
+    }
+
+    rec_convertebase(dec/base, base);
+    printf("%d", dec%base);
+}
+
+
+void it_convertebase(int dec, int base){
+    
+    int convertido[128]; 
+    int i = 127; 
+
+    while(dec >= base){
+        convertido[i] = dec%base; 
+        dec /= base; 
+        i--; 
+    }
+
+    convertido[i] = dec;
+   
+    for(; i < 128; i++){
+        printf ("%d", convertido[i]);
+    }
+}
 
 
 int rec_fibo(int p){
-    if (p == 1 || p == 0) {return 1;}
+    if (p < 2) {return p;}
 
     return (rec_fibo(p-1) + rec_fibo(p-2));
 }
 
+
 int it_fibo(int p){
     
-    if (p < 2){return 1;}
+    if (p < 2){return p;}
 
     int f[p]; 
-    f[0] = 1;
+    f[0] = 0;
     f[1] = 1;    
         
     for (int i = 2; i <= p; i++){
@@ -51,11 +86,13 @@ int it_fat(int n){
     return fat; 
 }
 
+
 int rec_fat(int n){
     if (n == 0){ return 1;}
 
     return n * rec_fat(n-1);
 }
+
 
 int rec_mult(int a, int b){
     if ((a && b) == 0) { return 0;}
@@ -63,11 +100,12 @@ int rec_mult(int a, int b){
     if (a == 1) return b;
 
     return b + rec_mult(a-1, b);
-
 }
+
 
 int it_mult(int a, int b){
     if ((a && b) == 0) return 0;
+
     int produto = b;
 
     for(; a > 1; a--){
@@ -76,12 +114,13 @@ int it_mult(int a, int b){
     return produto;
 }
 
+
 int rec_divide(int a, int b){
     if ((a < b) | (a && b) == 0) return 0;
 
     return rec_divide(a-b, b) + 1;
-
 }
+
 
 int it_divide(int a, int b){
     int quociente = 0;
@@ -92,19 +131,20 @@ int it_divide(int a, int b){
         a -= b;
         quociente++;
     }
+    
     return quociente;
 }
+
 
 void main(){
 
     clock_t tempo;
     tempo = clock();
 
-    int x = it_fibo(40);
+    it_convertebase(1500,16);
+    printf("\n");
 
     double exec = (clock() - tempo) / (double) CLOCKS_PER_SEC;
     
     printf("Tempo execucao: %9.f \n", exec);
-    printf("Valor: %d\n", x); 
-    
 }
