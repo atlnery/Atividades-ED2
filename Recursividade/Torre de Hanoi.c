b9634fb2108d9b9a2b69594d538ce4c7cc1ac74d
atlnery <49240604+atlnery@users.noreply.github.com>
2020-03-13 15:23:51 -0300

    Create Torre de Hanoi.c

---
+++ Recursividade/Torre de Hanoi.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+//EXECUTA O MÍNIMO DE POSSIBILIDADES - 2^N - 1
+
+int cont = 0;
+
+void hanoi(int n, char origem, char destino, char temp){
+        cont++;
+
+    if (n == 1){
+        printf("disco %d \t do pino %c ao pino %c\n", n, origem, destino);
+        return 0;
+    } 
+
+    hanoi(n-1, origem, temp, destino);
+    printf("disco %d \t do pino %c ao pino %c\n", n, origem, destino);
+    hanoi(n-1, temp, destino, origem);
+
+}
+
+void main(){
+
+    hanoi(2, 'O', 'D', 'T' );
+    printf("%d\n\n", cont);
+}
