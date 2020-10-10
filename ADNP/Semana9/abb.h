b0a79cb3575ac5cc3db28da21d0c8e580f78a3df
Amanda Lopes <49240604+atlnery@users.noreply.github.com>
2020-10-09 22:08:48 -0300

    Update abb.h

--- ADNP/Semana9/abb.h
+++ ADNP/Semana9/abb.h
@@ -3,6 +3,7 @@
 
     #include <stdio.h> 
     #include <stdlib.h>
+    #include <limits.h>
 
     typedef enum PRINT_TYPE{
         ORDEM = 1, 
@@ -16,15 +17,16 @@
         struct binary_search_tree* father;
         int key;
     }ABB; //Árvore Binária de Busca
-    
+
+
     ABB* ABB_search(ABB* no, int key);
     ABB* ABB_create(int key, ABB* left, ABB* right, ABB* father);
     ABB* ABB_insert(ABB** A, int key);
     static ABB* ABB_insert_father(ABB** A, int key, ABB* father);
 
     void ABB_print(ABB* no, PRINT_TYPE type);
-    static void ABB_print_pre_ordem(ABB* no, int num);
-    static void print_No(int key, int num);
+    static void ABB_print_pre_ordem(ABB* no, int num, char* pos);
+    static void print_No(int key, int num, char* pos);
 
     int ABB_size(ABB* no);
     void ABB_destroy(ABB* no); 
@@ -32,7 +34,5 @@
     int ABB_remove(ABB **no, int key);
     ABB* ABB_smaller_right(ABB *no);
     ABB* ABB_largest_left(ABB *no);
-
-
 
 #endif
